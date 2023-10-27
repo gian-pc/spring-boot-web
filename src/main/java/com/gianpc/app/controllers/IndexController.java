a0e9afad11724360b9f52157e4e2158821580823
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-10-26 21:04:51 -0500

    creando IndexController

---
+++ src/main/java/com/gianpc/app/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.gianpc.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
