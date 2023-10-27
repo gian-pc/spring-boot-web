0ae91ab3aad9c71ae8f8d8e57e2e8591eacc817b
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-10-26 23:37:26 -0500

    @GetMapping y rutas

--- src/main/java/com/gianpc/app/controllers/IndexController.java
+++ src/main/java/com/gianpc/app/controllers/IndexController.java
@@ -1,16 +1,16 @@
 package com.gianpc.app.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
+    @GetMapping({"/index","/principal","/"})
     public String index(){
         return "index";
     }
 
-    @RequestMapping("/home")
+    @GetMapping({"/home","/welcome","/casa"})
     public String home(){
         return "home";
     }
